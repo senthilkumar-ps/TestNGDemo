2e546cbe76cd1fa0b60edaed0aa8bbc98f9ff988
Senthilkumar <psysenthilkumar@gmail.com>
2021-04-22 13:39:25 +0530

    initial commit

---
+++ src/main/java/com/test/RandomEmailGenerator.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class RandomEmailGenerator  {
+	public String generate() {
+		return "feedback@yoursite.com";
+	}
+}
---
+++ src/test/java/com/test/TestRandomEmailGenerator.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestRandomEmailGenerator {
+
+	@Test()
+	public void testEmailGenerator() {
+
+		RandomEmailGenerator obj = new RandomEmailGenerator();
+		String email = obj.generate();
+
+		Assert.assertNotNull(email);
+		Assert.assertEquals(email, "feedback@yoursite.com");
+		Assert.assertTrue(true, "feedback@yoursite.com");
+//		Assert.assertFalse(true, "feedback");
+	}
+
+}
