b9bcd872f50a789d47c4a9d1141231395a30b21d
Senthilkumar <psysenthilkumar@gmail.com>
2021-04-23 16:24:51 +0530

    testng

--- src/test/java/com/test/TestRandomEmailGenerator.java
+++ src/test/java/com/test/TestRandomEmailGenerator.java
@@ -15,20 +15,28 @@ public class TestRandomEmailGenerator {
 		Assert.assertEquals(email, "feedback@yoursite.com");
 		Assert.assertTrue(true, "feedback@yoursite.com");
 		//		Assert.assertFalse(true, "feedback");
+	}
 
+	@Test()
+	public void testMailGenerator() {
+
+		RandomEmailGenerator obj = new RandomEmailGenerator();
 		String feed = obj.generateMail();
 
 		Assert.assertNotNull(feed);
 		Assert.assertEquals(feed, "feedback");
 		Assert.assertTrue(true, "feedback");
 
+	}
 
-		String name = obj.generateName();
+	@Test()
+	public void testNameGenerator() {
+		RandomEmailGenerator obj = new RandomEmailGenerator();
+		String feed = obj.generateName();
 
-		Assert.assertNotNull(name);
-		Assert.assertEquals(name, "abc");
+		Assert.assertNotNull(feed);
+		Assert.assertEquals(feed, "abc");
 		Assert.assertTrue(true, "abc");
 
 	}
-
 }
