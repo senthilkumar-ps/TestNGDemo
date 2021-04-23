8850d8f8e545232eee0920b1426aec787fdd1449
Senthilkumar <psysenthilkumar@gmail.com>
2021-04-23 16:02:29 +0530

    testng

--- src/main/java/com/test/RandomEmailGenerator.java
+++ src/main/java/com/test/RandomEmailGenerator.java
@@ -1,7 +1,17 @@
 package com.test;
 
 public class RandomEmailGenerator  {
+	public String generateMail() {
+		return "feedback@yoursite.com";
+	}
+	
+	public String generateName() {
+		return "feedback@yoursite.com";
+	}
+	
 	public String generate() {
 		return "feedback@yoursite.com";
 	}
+	
+	
 }
