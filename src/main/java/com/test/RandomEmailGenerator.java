c008df57c777fd14e0c9a1a52695b7bfdca4aebf
Senthilkumar <psysenthilkumar@gmail.com>
2021-04-28 11:01:34 +0530

    testng

--- src/main/java/com/test/RandomEmailGenerator.java
+++ src/main/java/com/test/RandomEmailGenerator.java
@@ -1,25 +1,27 @@
 package com.test;
 
 public class RandomEmailGenerator  {
-	
+
 	static final String FEEDBACK = "feedback";
 	static final String NAME = "abc";
 	static final String MAIL = "feedback@yoursite.com";
-	
-	
+
+
 	public String generateFeedback() {
 		return FEEDBACK;
 	}
-	
+
 	public String generateName() {
 		return NAME;
 	}
-	
+
 	public String generateMail() {
 		return MAIL;
 	}
-	
-	/*
-	 * public int addtionNum(int num1, int num2) { return num1*num2; }
-	 */
+
+	public int addtionNum(int num1, int num2) { 
+		
+		return num1*num2; 
+		
+	}
 }
